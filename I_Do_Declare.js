878729961157ef1cbd5712c7c98e1449cf00859d
Felipe <felipeo510.fs@gmail.com>
2017-08-21 13:12:58 -0700

    Declaring variables and assigning different data types

---
+++ I_Do_Declare.js
@@ -0,0 +1,24 @@
+var string_1 = "hello";
+var string_2 = "Coding";
+var string_3 = "Dojo";
+
+var firstNum = -200;
+var secondNum = 18;
+var thirdNum = 18.73;
+var lastNum = 0;
+
+var trueFalse = false;
+var falseTrue = true;
+
+var what;
+
+console.log("The First string is: " + string_1);
+console.log("The Second string is: "+ string_2);
+console.log("The Third string is: " + string_3);
+console.log("First Number: "+ firstNum);
+console.log("Second Number: "+ secondNum);
+console.log("Third Number: "+ thirdNum);
+console.log("Last Number: "+ lastNum);
+console.log("Is the earth Falt? "+ trueFalse);
+console.log("Do I Love Melissa? "+ falseTrue);
+console.log("huh?" + what);
